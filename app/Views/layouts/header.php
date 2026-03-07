d1f48e1a0b620bfe69781371e3c28567f9f2f12d
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2026-03-07 15:00:04 +0200

    languahge selector

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -10,11 +10,20 @@
 <body>
 <header class="top-nav">
     <div class="top-nav-inner">
-        <a class="brand" href="<?= base_url('/') ?>">Ticketing System</a>
-        <nav aria-label="Main navigation">
-            <ul class="menu">
-                <li><a class="menu-link is-active" href="<?= base_url('/') ?>">Κεντρική</a></li>
-            </ul>
-        </nav>
+        <div class="top-nav-left">
+            <a class="brand" href="<?= base_url('/') ?>">Ticketing System</a>
+            <nav aria-label="Main navigation">
+                <ul class="menu">
+                    <li><a class="menu-link is-active" href="<?= base_url('/') ?>">Κεντρική</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="lang-switcher">
+            <select id="language" name="language">
+                <option value="gr" selected>GR</option>
+                <option value="en">EN</option>
+            </select>
+        </div>
     </div>
 </header>
