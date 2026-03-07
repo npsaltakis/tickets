317be4b7fa3058477ead5504b5fed6111b739c27
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2026-03-07 14:41:18 +0200

    top bar

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -8,3 +8,13 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/styles.css') ?>">
 </head>
 <body>
+<header class="top-nav">
+    <div class="top-nav-inner">
+        <a class="brand" href="<?= base_url('/') ?>">Ticketing System</a>
+        <nav aria-label="Main navigation">
+            <ul class="menu">
+                <li><a class="menu-link is-active" href="<?= base_url('/') ?>">Κεντρική</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
